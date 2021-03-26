2e9954186f43bce0aba04fe8bd0355ba8677983a
BrunoFNRodrigues <bf17669@gmail.com>
2021-03-26 15:45:52 -0300

    Adiciona elementos necessários

---
+++ test/br/edu/insper/desagil/joice/model/TestJoice.java
@@ -0,0 +1,21 @@
+package br.edu.insper.desagil.joice.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestJoice {
+
+	@BeforeEach
+	public void setUp{
+		
+	}
+	
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
