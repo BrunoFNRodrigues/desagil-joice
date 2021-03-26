6fb7af7370623c9402f2cbdb2ca1920ca548c808
BrunoFNRodrigues <bf17669@gmail.com>
2021-03-26 18:10:02 -0300

    Adiciona  CalculadoraCCC

--- src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCCC.java
@@ -1,5 +1,16 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCCC {
-
+public class CalculadoraCCC extends Calculadora {
+	
+	public CalculadoraCCC(String nome){
+		super(nome);
+	}
+	
+	public double calcula(double peso, double raio) {
+		raio = raio*Math.pow(10, -7);
+		double volume = Math.pow((4*raio/Math.sqrt(3)),3);
+		double avogadro = 6.023*Math.pow(10,23);
+		double densidade = peso*2/(avogadro*volume);
+		return densidade;
+	}
 }
